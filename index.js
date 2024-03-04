ccaa6eee2413431a257ce46bafb1dc83b7895605
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-04 11:39:28 -0300

    move 'index.js' to root

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const cors = require("cors");
 
 const port = process.env.PORT || 3002;
 
-const router = require("./routes/index.routes.js");
+const router = require("./src/routes/index.routes.js");
 
-const { sequelize } = require("./database.js");
+const { sequelize } = require("./src/database.js");
 
 const server = express();
 
