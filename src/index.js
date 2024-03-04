3c0333f862ff360aaf61e46bf30f5628099e39ba
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-03 21:25:53 -0300

    respuesta 'html' a '/'

--- src/index.js
+++ src/index.js
@@ -14,37 +14,51 @@ const { sequelize } = require("./database.js");
 
 const server = express();
 
-const corsOptions = {
-  origin: "*",
-};
+// const corsOptions = {
+//   origin: "*",
+// };
 
-server.use(cors(corsOptions));
-server.use(bodyParser.json({ limit: "10mb", extended: true }));
-server.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
-server.use(morgan("dev"));
+// server.use(cors(corsOptions));
+// server.use(bodyParser.json({ limit: "10mb", extended: true }));
+// server.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+// server.use(morgan("dev"));
 
-// Imprimo en consola el pedido:
-/*   server.use((req, res, next) => {
-    console.log(`Request: ${req.method} ${req.url}`);
-    // console.log(req);
-    next(); }); */
+// // Imprimo en consola el pedido:
+// /*   server.use((req, res, next) => {
+//     console.log(`Request: ${req.method} ${req.url}`);
+//     // console.log(req);
+//     next(); }); */
 
-server.use("/", router);
+// server.use("/", router);
 
-server.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send({ error: err.message });
-});
+// server.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).send({ error: err.message });
+// });
 
-sequelize
-  .sync({ alter: true })
-  // .sync({ force: true }) // para resetear la base de datos Force: true
-  .then(() => {
-    console.log(`Database & tables created`);
-    server.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+// sequelize
+//   .sync({ alter: true })
+//   // .sync({ force: true }) // para resetear la base de datos Force: true
+//   .then(() => {
+//     console.log(`Database & tables created`);
+//     server.listen(port, () => {
+//       console.log(`Server is listening on port ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+  server.get('/', (req, res) => {
+    const htmlResponse =
+    `<html>
+        <head>
+            <title>Hello World!</title>
+        </head>
+        <body>
+         <h1>Hello World!</h1>
+         </body>
+         </html>`;
+
+    res.send(htmlResponse);
   });
