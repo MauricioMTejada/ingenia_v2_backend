7f93be8fad3cd903e9b0b2ffe673eee3eb910f98
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-05 16:13:45 -0300

    modularize database_test

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
-require("dotenv").config();
 
-const express = require("express");
+// importations
+	require("dotenv").config();
+	const express = require("express");
+	const bodyParser = require("body-parser");
+	const morgan = require("morgan");
+	const cors = require("cors");
+	// const pg = require("pg");
+	const router = require("./src/routes/index.routes.js");
+	const { Result } = require("express-validator");
+	// const { sequelize } = require("./src/database.js");
+	const { pool } = require("./src/database_test/database_test.js")
 
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const cors = require("cors");
-const pg = require("pg");
+// constants
+	const port = process.env.PORT || 3002;
+	const server = express();
+	const corsOptions = { origin: "*", };
 
-const port = process.env.PORT || 3002;
+// 'use'
+	server.use(cors(corsOptions));
+	server.use(bodyParser.json({ limit: "10mb", extended: true }));
+	server.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+	server.use(morgan("dev"));
+	server.use("/", router);
 
-const router = require("./src/routes/index.routes.js");
-const { Result } = require("express-validator");
+// print in console the request
+	// server.use((req, res, next) => {
+	// 	console.log(`Request: ${req.method} ${req.url}`);
+	// 	console.log(req);
+	// 	next();
+	// });
 
-// const { sequelize } = require("./src/database.js");
-
-const server = express();
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  // ssl: true
-})
-const corsOptions = {
-  origin: "*",
-};
-
-server.use(cors(corsOptions));
-server.use(bodyParser.json({ limit: "10mb", extended: true }));
-server.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
-server.use(morgan("dev"));
-
-// Imprimo en consola el pedido:
-/*   server.use((req, res, next) => {
-    console.log(`Request: ${req.method} ${req.url}`);
-    // console.log(req);
-    next(); }); */
-
-server.use("/", router);
-
-// server.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).send({ error: err.message });
-// });
+// print errors in console
+	// server.use((err, req, res, next) => {
+	//   console.error(err);
+	//   res.status(500).send({ error: err.message });
+	// });
 
 // sequelize
 //   .sync({ alter: true })
@@ -55,11 +49,13 @@ server.use("/", router);
 //     console.log(err);
 //   });
 
-server.get('/ping', async (req, res) => {
-  const result = await pool.query('SELECT NOW()')
-  return res.json(result.rows[0].now)
-})
+// test database
+	server.get('/ping', async (req, res) => {
+		const result = await pool.query('SELECT NOW()')
+		return res.json(result.rows[0].now)
+	})
 
-  server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+// test database - listen server
+	server.listen(port, () => {
+		console.log(`Server running on port ${port}`);
+	});
