88494c0c25f27de11dfcd59e7b44a3b2f68c42af
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-04 21:03:32 -0300

    comment lost dependences

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const express = require("express");
 
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const morgan = require("morgan");
+// const cors = require("cors");
 
 const port = process.env.PORT || 3002;
 
