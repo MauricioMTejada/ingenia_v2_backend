e51b7e2736e6fb6f2f979148d46391fa5457d8ee
MauricioTejada <mauriciotejada14@gmail.com>
2024-03-06 17:03:14 -0300

    finish 'server_production.js'

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// 	Nombre original del archivo:
+// 	server_production.js
+
 // importations
 	require("dotenv").config();
 	const express = require("express");
 	const bodyParser = require("body-parser");
 	const morgan = require("morgan");
 	const cors = require("cors");
-	const router = require("./src/routes/test_index.routes.js");
-	const { Result } = require("express-validator");
-	// const { sequelize } = require("./src/database.js");
-	const { sequelize, } = require("./src/test.database.route/database_test.js");
-	const { pool, } = require("./src/test.database.route/database_test_(without_sequelize).js");
+	const { cyan } = require('colorette');
+	const { sequelize, } = require("./src/test.database.route/database_test.js"); // editar con la nueva base de datos
+	const router = require("./src/routes/test_index.routes.js"); // editar con el nuevo router
 
 // constants
 	const port = process.env.PORT || 3002;
@@ -23,13 +24,13 @@
 	server.use("/", router);
 
 // print in console the request
-server.use((req, res, next) => {
-	console.log(`Request: ${req.method} ${req.url}`);
-	// console.log(`Body: ${req.body}`);
-	console.log(req.body);
-	// console.log(req);
-	next();
-});
+	server.use((req, res, next) => {
+		console.log(`Request: ${req.method} ${req.url}`);
+		// console.log(`Body: ${req.body}`);
+		console.log(req.body);
+		// console.log(req);
+		next();
+	});
 
 // print errors in console
 // server.use((err, req, res, next) => {
@@ -37,39 +38,11 @@ server.use((req, res, next) => {
 //   res.status(500).send({ error: err.message });
 // });
 
-async function startServer() {
-	try {
-		await sequelize.sync({ force: true }); // Para resetear DB: "force: true"
+
+// Base de Datos:
+	sequelize.sync({ force: true }); // Para resetear DB: "force: true"
 		console.log(`Database & tables created`);
 		server.listen(port, () => {
+			console.log(cyan('Server Ingenia v2'));
 			console.log(`Server is listening on port ${port}`);
 		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-startServer(); // |=> comentar para probar la BD
-
-
-// Test - Base de datos sin Sequelize
-async function test_database() {
-	try {
-		//   await sequelize.sync({ force: true }); // Para resetear la base de datos: force: true
-		//   console.log(`Database & tables created`);
-
-		server.get("/ping", async (req, res) => {
-			const result = await pool.query("SELECT NOW()");
-			return res.json(result.rows[0].now);
-		});
-
-		server.listen(port, () => {
-			console.log(`Test Database without sequelize`);
-			console.log(`Server is listening on port ${port}`);
-		});
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-// test_database();	// |=> descomentar para probar la BD
